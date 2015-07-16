b798d570c884d85ddece37351fa80098328eb921
Jessica <jessicakoch136@icloud.com>
2015-07-16 10:57:55 -0600

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,27 +11,42 @@ class User < ActiveRecord::Base
   has_many :skills, dependent: :destroy
   has_many :educations, dependent: :destroy
 
-  def self.create_with_omniauth(auth, code)
-    @current = create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["name"]
-      user.location = auth["info"]["location"]["name"]
-      user.linkedin_url = auth["info"]["urls"]["public_profile"]
-      user.picture_url = auth["info"]["image"]
-      user.email = auth["info"]["email"]
-      user.description = auth["info"]["description"]
-      user.token = auth["credentials"]["token"]
-    end
-    get_info = LinkedinFetcher.new(@current)
-    get_info.get_skills
-    get_info.get_education
-    get_info.get_positions
-    get_info.get_past_positions
-    return @current
+  def self.from_omniauth(auth_hash)
+    user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+    user.name = auth_hash['info']['name']
+    user.location = auth_hash['info']['location']
+    user.linkedin_url = auth_hash['info']['linkedin_url']
+    user.image = auth_hash['info']['image']
+    user.email = auth_hash['info']['email']
+    user.bio = auth_hash['info']['bio']
+    user.token = auth_hash['info']['token']
+    user.save!
+    user
   end
 
-  def current_employer
-    employments.where(current: true).first
+
+
+    # def self.create_with_omniauth(auth, code)
+    #   @current = create! do |user|
+    #     user.provider = auth["provider"]
+    #     user.uid = auth["uid"]
+    #     user.name = auth["info"]["name"]
+    #     user.location = auth["info"]["location"]["name"]
+    #     user.linkedin_url = auth["info"]["urls"]["public_profile"]
+    #     user.picture_url = auth["info"]["image"]
+    #     user.email = auth["info"]["email"]
+    #     user.description = auth["info"]["description"]
+    #     user.token = auth["credentials"]["token"]
+    #   end
+    #   get_info = AngellistFetcher.new(@current)
+    #   get_info.get_skills
+    #   get_info.get_education
+    #   get_info.get_positions
+    #   get_info.get_past_positions
+    #   return @current
+    # end
+
+    def current_employer
+      employments.where(current: true).first
+    end
   end
-end
